ebec7d46539746c3c35bf98aa84b3822ec4342ff
pangamma <pro.tayloralove@gmail.com>
2021-08-02 21:18:30 -0700

    remove TKF and random unused component

--- app/src/__tkf/main.ts
+++
@@ -1,8 +0,0 @@
-import { DataJsHref } from './global-js/dataJsHref';
-import './global-js/focus';
-import './scss/main.scss';
-import './global-js/js-clickOnSpaceOrEnter';
-
-export const initializeTKF = () => {
-    DataJsHref.onEnable();
-};
